1884b0c2439d2a98b4be13d3a2176223aadfd4f6
s2 <samueldf83@gmail.com>
2023-04-17 21:25:20 +0200

    *

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ export default function Home() {
   const [name, setName] = useState('')
   const [amount, setAmount] = useState('')
   const [selectedCategory, setSelectedCategory] = useState('')
+  const [filteredProducts, setFilteredProducts] = useState([])
 
   useEffect(() => {
     const unsubscribe = onSnapshot(collection(db, 'productos'), snapshot => {
@@ -32,15 +33,25 @@ export default function Home() {
     }
   }
 
+  useEffect(() => {
+    setFilteredProducts(products)
+  }, [products])
+
+  const handleNameFilter = e => {
+    const value = e.target.value.toLowerCase()
+    const filtered = products.filter(item => item.name.toLowerCase().includes(value))
+    setFilteredProducts(filtered)
+  }
+
   const uniqueCategories = {}
 
   return (
-    <main className='container mx-auto'>
+    <main className='container mx-auto pb-60'>
       <h1 className='flex items-center justify-center border-b border-green-500 bg-green-100 p-4 text-2xl font-bold uppercase'>
         Mercadona
       </h1>
 
-      {products.map(({ id, name, amount, category }) => {
+      {filteredProducts.map(({ id, name, amount, category }) => {
         if (!(category in uniqueCategories)) {
           uniqueCategories[category] = true
           return (
@@ -66,13 +77,16 @@ export default function Home() {
         }
       })}
 
-      <footer className='sticky bottom-0 z-10 flex w-full flex-col items-center gap-4 bg-slate-500 px-4 py-2'>
+      <footer className='fixed bottom-0 z-10 flex w-full flex-col items-center gap-4 bg-slate-500 px-4 py-2'>
         <input
           className='flex w-full items-center justify-center rounded-lg bg-slate-50 px-4 py-2 shadow-md'
           type='text'
           placeholder='Nombre'
           value={name}
-          onChange={e => setName(e.target.value)}
+          onChange={e => {
+            setName(e.target.value)
+            handleNameFilter(e)
+          }}
         />
         <div className='flex gap-4'>
           <input
